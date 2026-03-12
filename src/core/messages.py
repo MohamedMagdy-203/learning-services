ce0f2635e57206e6175967c2232764b06c321ac1
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-12 22:54:49 +0200

    chore: updated with ferching roadmap messages

--- src/core/messages.py
+++ src/core/messages.py
@@ -1,2 +1,4 @@
 WELCOME_MESSAGE = "Welcome to Learning Services API"
 SERVER_RUNNING_SUCCESSFULY = "The server is running successfuly"
+FETCH_ROADMAP_CONTEXT_SUCCESSFULY = "The roadmap context fetched successfuly"
+FETCH_ROADMAP_CONTEXT_ERROR = "Failed to fetch roadmap context"
