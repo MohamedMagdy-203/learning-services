c9122a9f4a5977c7f2dd09dd0f2a3fab9f7c328f
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-12 22:52:36 +0200

    chore: updated with adding the MAIN_BACKEND_URL

--- src/core/config.py
+++ src/core/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     APP_NAME: str = "Learning Services"
     APP_VERSION: str = "1.0.0"
+    MAIN_BACKEND_URL: str = "http://localhost:3000"
 
 
 @lru_cache
