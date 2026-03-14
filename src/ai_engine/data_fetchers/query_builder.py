faa9b921c00ee3874c1e36189409b13ceef8970f
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-14 06:57:15 +0200

    feat: Creating a search query that passed to tavily

---
+++ src/ai_engine/data_fetchers/query_builder.py
@@ -0,0 +1,19 @@
+from src.models.schemas import RoadmapGenerationRequest
+
+
+def query_builder(requested_data: RoadmapGenerationRequest) -> str:
+    # User Info
+    track_name = requested_data.user_profile_schema.tracks[0]
+
+    # Subtopic Info
+    subtopic_name = requested_data.target_subtopic_schema.Name
+    subtopic_description = requested_data.target_subtopic_schema.Description
+    subtopic_difficulty = requested_data.target_subtopic_schema.Difficulty
+
+    search_query = (
+        f"{subtopic_name} {subtopic_description} "
+        f"{subtopic_difficulty} level "
+        f"guide tutorial explanation documentation examples "
+        f"{track_name} "
+    )
+    return search_query
