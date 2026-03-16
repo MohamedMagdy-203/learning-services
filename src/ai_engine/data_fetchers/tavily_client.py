b84788088adcc0aba1f86add6ea1d9c81b8eaff4
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-16 03:00:40 +0200

    fix: editing the output of the tavily to be every source independant

--- src/ai_engine/data_fetchers/tavily_client.py
+++ src/ai_engine/data_fetchers/tavily_client.py
@@ -16,22 +16,24 @@ async def fetch_subtopic_content(
         search_string = query_builder(requested_data)
         logger.info("Tavily search started | query: %s", search_string)
 
-        content: str = ""
-        sources: List[Dict[str, str]] = []
         tavily_client = AsyncTavilyClient(api_key=settings.TAVILY_API_KEY)
         tavily_response: Dict[str, Any] = await tavily_client.search(  # type: ignore
             query=search_string, include_raw_content=True, timeout=10.0
         )
 
-        for result in tavily_response.get("results", []):
-            content += (result.get("raw_content") or "") + "\n\n"
-            sources.append({"title": result.get("title"), "url": result.get("url")})
+        results_list: List[Dict[str, str]] = []
 
-        logger.info("Tavily search done | sources found: %d", len(sources))
-        return {
-            "content": content,
-            "sources": sources,
-        }
+        for result in tavily_response.get("results", []):
+            results_list.append(
+                {
+                    "title": result.get("title") or "",
+                    "url": result.get("url") or "",
+                    "raw_content": result.get("raw_content") or "",
+                }
+            )
+
+        logger.info("Tavily search done | sources found: %d", len(results_list))
+        return {"results": results_list}
     except Exception as exc:
         logger.exception("Tavily call failed | query: %s", search_string)  # type: ignore
         raise TavilyCallingError(TavilyCallingError.DEFAULT_MESSAGE) from exc
