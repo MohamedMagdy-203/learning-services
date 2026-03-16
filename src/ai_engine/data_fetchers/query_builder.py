c63237cb27a67bc5492eda2b49ef2c1e830c7c15
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-16 06:59:04 +0200

    fix: enhance the query builder for needed results

--- src/ai_engine/data_fetchers/query_builder.py
+++ src/ai_engine/data_fetchers/query_builder.py
@@ -2,19 +2,15 @@ from src.models.schemas import RoadmapGenerationRequest
 
 
 def query_builder(requested_data: RoadmapGenerationRequest) -> str:
-    # User Info
     tracks = requested_data.user_profile_schema.tracks
     track_name = tracks[0] if tracks else "General"
 
-    # Subtopic Info
     subtopic_name = requested_data.target_subtopic_schema.Name
-    subtopic_description = requested_data.target_subtopic_schema.Description
     subtopic_difficulty = requested_data.target_subtopic_schema.Difficulty
 
     search_query = (
-        f"{subtopic_name} {subtopic_description} "
-        f"{subtopic_difficulty} level "
-        f"guide tutorial explanation documentation papers examples"
-        f"{track_name} "
+        f"{subtopic_name} {subtopic_difficulty} level "
+        f"tutorial guide course "
+        f"for {track_name} developers"
     )
     return search_query
