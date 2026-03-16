2af038cb6c54e83a7910846ebe6bc94029c7c218
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-16 03:02:36 +0200

    fix: remove the messages that already been removed in the messages.py file

--- tests/test_main_backend_client.py
+++ tests/test_main_backend_client.py
@@ -4,21 +4,19 @@ import httpx
 from src.core.config import Settings
 from src.services.main_backend_client import fetch_roadmap_context
 from src.core.exceptions import FetchRoadmapContextError
-from src.core.messages import FETCH_ROADMAP_CONTEXT_SUCCESSFULLY
 from src.core.mock_data import MOCK_VALID_RESPONSE
 
 
 @pytest.mark.asyncio
 @respx.mock
 async def test_fetch_roadmap_success():
-    settings = Settings()
+    settings = Settings()  # type: ignore
     user_id = "user_123"
     subtopic_id = "sub_456"
     url = f"{settings.MAIN_BACKEND_URL}/api/internal/roadmap-context/{user_id}/{subtopic_id}"
     respx.get(url).mock(return_value=httpx.Response(200, json=MOCK_VALID_RESPONSE))
     result = await fetch_roadmap_context(user_id, subtopic_id, settings)
 
-    assert result["status"] == FETCH_ROADMAP_CONTEXT_SUCCESSFULLY
     assert result["roadmap_context"].user_profile_schema.id == "user_123"
     assert (
         result["roadmap_context"].target_subtopic_schema.Name == "Async/Await in Python"
@@ -28,7 +26,7 @@ async def test_fetch_roadmap_success():
 @pytest.mark.asyncio
 @respx.mock
 async def test_fetch_roadmap_raises_error():
-    settings = Settings()
+    settings = Settings()  # type: ignore
     user_id = "user_123"
     subtopic_id = "sub_456"
     url = f"{settings.MAIN_BACKEND_URL}/api/internal/roadmap-context/{user_id}/{subtopic_id}"
