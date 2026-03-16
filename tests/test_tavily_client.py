f5be463a44503538ba9be907f4a0784869c8bf5b
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-16 07:07:41 +0200

    chore: updating the test with the changes have been done in mock data and outcomes

--- tests/test_tavily_client.py
+++ tests/test_tavily_client.py
@@ -13,7 +13,14 @@ mock_settings = Settings(TAVILY_API_KEY="test_key_123")
 @pytest.mark.asyncio
 @patch("src.ai_engine.data_fetchers.tavily_client.AsyncTavilyClient")
 async def test_fetch_subtopic_content_success(mock_tavily_class: MagicMock):
-    mock_tavily_class.return_value.search = AsyncMock(return_value=MOCK_TAVILY_RESPONSE)
+    mock_tavily_class.return_value.search = AsyncMock(
+        side_effect=[
+            MOCK_TAVILY_RESPONSE,
+            {"results": []},
+            {"results": []},
+        ]
+    )
+
     result = await tavily_client.fetch_subtopic_content(
         settings=mock_settings, requested_data=mock_requested_data
     )
