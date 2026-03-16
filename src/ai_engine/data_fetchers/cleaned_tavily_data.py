41d4d93de0db84503a3775832854828d3a7d8899
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-03-16 07:04:23 +0200

    feat: creating a process for clean the data and store this cleaned data to be used

---
+++ src/ai_engine/data_fetchers/cleaned_tavily_data.py
@@ -0,0 +1,28 @@
+from typing import Dict, Any
+from src.core.config import Settings
+from src.models.schemas import RoadmapGenerationRequest
+from src.ai_engine.data_fetchers.tavily_client import fetch_subtopic_content
+from src.ai_engine.data_fetchers.data_cleaner import clean_content
+
+
+async def fetch_and_clean_subtopic_content(
+    settings: Settings,
+    requested_data: RoadmapGenerationRequest,
+) -> list[Dict[str, Any]]:
+    raw_results = await fetch_subtopic_content(
+        settings=settings,
+        requested_data=requested_data,
+    )
+
+    cleaned_results = []
+    for result in raw_results["results"]:
+        cleaned_content = clean_content(result["raw_content"])
+        cleaned_results.append(
+            {  # type: ignore
+                "title": result["title"],
+                "url": result["url"],
+                "raw_content": cleaned_content,
+            }
+        )
+
+    return cleaned_results  # type: ignore
