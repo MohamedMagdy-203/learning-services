4af0139c4c4b27a5155a675922ae62f37f9e4480
Alaayasser6 <alaayasser862@gmail.com>
2026-04-07 02:24:17 +0200

    feat: register mindmap API router in main application

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from .routers import base, data
+from .routers import base, data , mindmap
 
 app = FastAPI()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(mindmap.mindmap_router)
