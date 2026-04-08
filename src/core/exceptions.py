ebe20b7983a116b2969dacd14da236e7f8c6dea9
HaneenMohamed25 <haneenmohamedelbasyony@gmail.com>
2026-04-08 19:14:27 +0200

    chore:updating exceptions file

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -4,3 +4,10 @@ class FetchRoadmapContextError(Exception):
 
 class TavilyCallingError(Exception):
     DEFAULT_MESSAGE = "There is an error at calling Tavily"
+class NoContentFoundError(Exception):
+    """Raised when no content is found for a given URL."""
+    pass
+
+class DistillationError(Exception):
+    """Raised when LLM fails to distill content."""
+    pass
