8936d23883890486afd1155b22141e6d51d9f9ba
HaneenMohamed25 <haneenmohamedelbasyony@gmail.com>
2026-04-08 19:25:20 +0200

    chore:edit main file to include dist_router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from .routers import base, data
+from src.routers import base, data
+from src.routers.distillation_router import dist_router
 
 app = FastAPI()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+
+
+app.include_router(dist_router)
+
