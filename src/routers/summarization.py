80d827c8e42f2397e7bcdb4b4eab709cc25b3516
Hana Amr <hanaamrmahmood.1@gmail.com>
2026-04-09 16:05:31 +0200

    feat: connect to Qdrant, fetch top 3 sections, and summarize them in summarizer.py

--- src/routers/summarization.py
+++ src/routers/summarization.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.services.summarizer import summarize_text
+from src.services.summarizer import summarize_text, fetch_context_from_qdrant
 from src.models.schemas import SummarizationRequest
 import asyncio
 
@@ -8,12 +8,20 @@ summarization_router = APIRouter()
 
 @summarization_router.post("/summarize/")
 async def summarize_endpoint(request: SummarizationRequest):
-    import asyncio
     
     loop = asyncio.get_running_loop()
+
+    fetched_context = await loop.run_in_executor(
+        None,
+        fetch_context_from_qdrant,
+        request.title
+    )
+
+
     result = await loop.run_in_executor(
         None,
         summarize_text,
+        fetched_context,
         request.page_content,
         request.title,
         request.max_length,
--- src/services/summarizer.py
+++ src/services/summarizer.py
@@ -1,5 +1,33 @@
+from qdrant_client import QdrantClient
+from src.core.config import get_settings
+from sentence_transformers import SentenceTransformer
 from functools import lru_cache
 from transformers import pipeline
+
+
+settings = get_settings()
+client = QdrantClient(url=settings.QDRANT_URL)
+collection_name = settings.QDRANT_COLLECTION_NAME
+
+
+embed_model = SentenceTransformer("paraphrase-multilingual-mpnet-base-v2")
+
+
+def fetch_context_from_qdrant(query_text: str, top_k: int = 3) -> str:
+    query_vector = embed_model.encode(query_text).tolist()
+
+    results = client.search(
+        collection_name=collection_name,
+        query_vector=query_vector,
+        limit=top_k
+    )
+
+    context_texts = [
+        r.payload["page_content"] for r in results if "page_content" in r.payload
+    ]
+
+    return "\n\n".join(context_texts)
+
 
 
 @lru_cache(maxsize=1)
@@ -20,14 +48,14 @@ Your summary should:
 - Mention the source title and type at the end
 
 Content:
-{}
+{context_texts}
 """
 
-def summarize_text(page_content: str, title: str, max_length: int = 350, min_length: int = 50) -> str:
+def summarize_text(context_texts: str, title: str, max_length: int = 350, min_length: int = 50) -> str:
     """
     Summarize the page content with context from the title.
     """
     summarizer = get_summarizer()
-    prompt = prompt_template.format(page_content, title=title)
+    prompt = prompt_template.format(context_texts, title=title)
     summary = summarizer(prompt, max_length=max_length, min_length=min_length)
     return summary[0]["generated_text"]
