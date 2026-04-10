afe110e9be3c9edb53113833dc5737a44e4a918f
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-04-10 14:32:17 +0200

    chore: edit the tavily query to be more accurate by removing the track name

--- src/ai_engine/data_fetchers/query_builder.py
+++ src/ai_engine/data_fetchers/query_builder.py
@@ -2,15 +2,10 @@ from src.models.schemas import RoadmapGenerationRequest
 
 
 def query_builder(requested_data: RoadmapGenerationRequest) -> str:
-    tracks = requested_data.user_profile_schema.tracks
-    track_name = tracks[0] if tracks else "General"
-
     subtopic_name = requested_data.target_subtopic_schema.Name
     subtopic_difficulty = requested_data.target_subtopic_schema.Difficulty
 
     search_query = (
-        f"{subtopic_name} {subtopic_difficulty} level "
-        f"tutorial guide course "
-        f"for {track_name} developers"
+        f"{subtopic_name} {subtopic_difficulty} level " f"tutorial guide course "
     )
     return search_query
