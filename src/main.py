67fc4094770f8ca08adb78d33a79d60f72dd5483
HaneenMohamed25 <haneenmohamedelbasyony@gmail.com>
2026-04-16 19:36:50 +0200

    chore:include summ_router and make  functons to initialize &close clients

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
-from .routers import base, data,summarization
+from .routers import base, data, summarization_router
+from src.ai_engine.summarization_engine.openai_client_dependency import (
+    init_openai_client,
+    close_openai_client,
+)
+
+from src.ai_engine.data_fetchers.qdrant_client_dependency import (
+    init_qdrant_client,
+    close_qdrant_client,
+)
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+async def startup_event():
+    await init_openai_client()
+    await init_qdrant_client()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await close_openai_client()
+    await close_qdrant_client()
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
-app.include_router(summarization.summarization_router)
 
-
+app.include_router(summarization_router.summ_router)
