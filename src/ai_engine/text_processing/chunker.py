84a55b759426b232cec85130eea860cd15ed4ce3
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-04-16 19:06:01 +0200

    fix: remove Recursive text splitter for more accurate result and small number of chuncks

--- src/ai_engine/text_processing/chunker.py
+++ src/ai_engine/text_processing/chunker.py
@@ -1,17 +1,8 @@
 import logging
 from langchain_experimental.text_splitter import SemanticChunker  # type: ignore
-from langchain_text_splitters import RecursiveCharacterTextSplitter  # type: ignore
 from src.ai_engine.vector_store.embedder import get_embeddings  # type: ignore
 
 logger = logging.getLogger(__name__)
-
-MAX_CHAR_LIMIT = 1500
-
-_fallback_splitter = RecursiveCharacterTextSplitter(  # type: ignore
-    chunk_size=MAX_CHAR_LIMIT,
-    chunk_overlap=150,
-    separators=["\n\n", "\n", ".", "،", " ", ""],
-)
 
 
 def chunk_text(raw_content: str) -> list[str]:
@@ -29,15 +20,7 @@ def chunk_text(raw_content: str) -> list[str]:
         breakpoint_threshold_amount=85,
     )
 
-    semantic_chunks = splitter.split_text(raw_content)  # type: ignore
-    final_chunks = []
-
-    for chunk in semantic_chunks:  # type: ignore
-        if len(chunk) > MAX_CHAR_LIMIT:  # type: ignore
-            sub_chunks = _fallback_splitter.split_text(chunk)  # type: ignore
-            final_chunks.extend(sub_chunks)  # type: ignore
-        else:
-            final_chunks.append(chunk)  # type: ignore
+    final_chunks = splitter.split_text(raw_content)  # type: ignore
 
     logger.info("Chunking done | chunks produced: %d", len(final_chunks))  # type: ignore
     return final_chunks  # type: ignore
