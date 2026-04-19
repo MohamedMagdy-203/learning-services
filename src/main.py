bb43075f052f0ad5d6b745b7489ce559bb10f91d
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-04-19 19:18:47 +0200

    chore: update the roadmap router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from .routers import base, data, mindmap
+from .routers import base, roadmap, mindmap
 
 app = FastAPI()
 
 app.include_router(base.base_router)
-app.include_router(data.data_router)
+app.include_router(roadmap.roadmap_router)
 app.include_router(mindmap.mindmap_router)
