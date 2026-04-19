a6edc2fc7a3cfb67b77431c2e83e2a9f124d4a63
Mohamed Magdy <mohamedmagdyrizk203@gmail.com>
2026-04-19 19:17:32 +0200

    chore: add hugging face api key to setting configration

--- src/core/config.py
+++ src/core/config.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):  # type: ignore
     QDRANT_URL: str = "http://localhost:6333"
     QDRANT_COLLECTION_NAME: str = "learning_materials"
     GEMINI_RERANKER_TIMEOUT_MS: int = 60000
+    hf_token: str | None = None
 
 
 @lru_cache
