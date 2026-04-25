05952dbbdd3ea968300f7d3b0675e51ef9f91bec
HaneenMohamed25 <haneenmohamedelbasyony@gmail.com>
2026-04-25 23:27:35 +0300

    chore:add quiz router in main and delete old data router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from src.routers import base, data
-from src.routers.BankQuestions_router import gen_router
-from src.ai_engine.BankQuestions_engine.openai_client_dependency import (
+from src.routers import base
+from src.routers.quiz_routers import gen_router, quiz_router
+from src.ai_engine.quiz_feature.BankQuestions_engine.openai_client_dependency import (
     init_openai_client,
     close_openai_client,
 )
@@ -11,6 +11,7 @@ from src.ai_engine.data_fetchers.qdrant_client_dependency import (
     init_qdrant_client,
     close_qdrant_client,
 )
+from src.ai_engine.quiz_feature.quiz_qdrant_client import ensure_quiz_collection_exists
 
 app = FastAPI()
 
@@ -20,8 +21,10 @@ async def startup_event():
     await init_openai_client()
     try:
         await init_qdrant_client()
+        await ensure_quiz_collection_exists()
     except Exception:
         await close_openai_client()
+        await close_qdrant_client()
         raise
 
 
@@ -34,9 +37,11 @@ async def shutdown_event():
 
 
 app.include_router(base.base_router)
-app.include_router(data.data_router)
+
 
 app.include_router(gen_router)
+app.include_router(quiz_router)
+
 app.include_router(summarization_router.summ_router)
 app.include_router(roadmap.roadmap_router)
 app.include_router(mindmap.mindmap_router)
